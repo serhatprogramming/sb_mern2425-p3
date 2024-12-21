6b001f1e5df60b2a64d7f20c5ee7366b04e2f32b
serhatprogramming <serhatprogramming@gmail.com>
2024-12-21 18:12:46 -0500

    post test added.

--- part3-notes-backend/tests/note_api.test.js
+++ part3-notes-backend/tests/note_api.test.js
@@ -1,19 +1,63 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach, before } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const connectToDB = require("../utils/db");
+const Note = require("../models/note");
 
 // connectToTestingDB
 connectToDB();
+const initialNotes = [
+  { content: "HTML is easy", important: true },
+  { content: "CSS is tricky", important: false },
+];
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let note = new Note(initialNotes[0]);
+  await note.save();
+  note = new Note(initialNotes[1]);
+  await note.save();
+});
 
 test("notes are returned as JSON", async () => {
   await api
     .get("/api/notes")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("two notes in the DB", async () => {
+  const response = await api.get("/api/notes").expect(200);
+  assert.strictEqual(response.body.length, initialNotes.length);
+});
+
+test("first note is about HTTP", async () => {
+  const { body } = await api.get("/api/notes");
+  assert.strictEqual(body[0].content.includes("HTML"), true);
+});
+
+test("A valid note can be added", async () => {
+  const note = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
+  const newNote = await api
+    .post("/api/notes")
+    .send(note)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+  const content = response.body.map((n) => n.content);
+  assert.strictEqual(initialNotes.length + 1, response.body.length);
+  assert.strictEqual(
+    content.includes("async/await simplifies making async calls"),
+    true
+  );
 });
 
 after(async () => {
