0e47032658129476dbcb864605f6baad54663d10
serhatprogramming <serhatprogramming@gmail.com>
2024-12-20 22:22:33 -0500

    logger expanded.

--- part3-notes-backend/utils/logger.js
+++ part3-notes-backend/utils/logger.js
@@ -1,9 +1,13 @@
 const info = (...params) => {
-  console.log(...params);
+  console.log("[INFO]", ...params);
 };
 
 const error = (...params) => {
-  console.error(...params);
+  console.error("[ERROR]", ...params);
 };
 
-module.exports = { info, error };
+const debug = (...params) => {
+  console.log("[DEBUG]", ...params);
+};
+
+module.exports = { info, error, debug };
