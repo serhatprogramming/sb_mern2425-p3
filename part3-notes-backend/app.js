5ce8ab42485cf5ab8fd9bc822c59ff5f8eaf2a98
serhatprogramming <serhatprogramming@gmail.com>
2024-12-30 13:29:38 -0500

    users router coded and imported in app.js

--- part3-notes-backend/app.js
+++ part3-notes-backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const notesRouter = require("./routes/notes");
+const usersRouter = require("./routes/users");
 const middleware = require("./utils/middleware");
 
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
