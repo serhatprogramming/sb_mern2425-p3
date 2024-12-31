29993583ebd09a2d593c6d6e32464b190b37b38c
serhatprogramming <serhatprogramming@gmail.com>
2024-12-31 16:38:35 -0500

    Togglable update.

--- part3-notes-frontend/src/components/Togglable.jsx
+++ part3-notes-frontend/src/components/Togglable.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false);
-
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
   const toggleVisibility = () => {
     setVisible(!visible);
   };
@@ -14,10 +15,10 @@ const Togglable = forwardRef((props, refs) => {
 
   return (
     <div>
-      <div style={{ display: visible ? "none" : "" }}>
+      <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={{ display: visible ? "" : "none" }}>
+      <div style={showWhenVisible}>
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
