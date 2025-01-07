036df637dd475ed0464825dd33ef83724aef3c1f
serhatprogramming <serhatprogramming@gmail.com>
2025-01-07 13:32:39 -0500

    reducer and store added to App component only.

--- part5-State-Management/simple-notes-redux/src/App.jsx
+++ part5-State-Management/simple-notes-redux/src/App.jsx
@@ -1,7 +1,47 @@
+import { createStore } from "redux";
+
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case "NEW_NOTE": {
+      return state.concat(action.payload);
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the app stats is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 1,
+  },
+});
+
 const App = () => {
   return (
     <div>
       <h2>Simple Notes App with Redux</h2>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content}{" "}
+            <strong>({note.important ? "important" : "not important"})</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
