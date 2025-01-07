8b580e1744e9b774f4de621ee51dfd8b817fba8f
serhatprogramming <serhatprogramming@gmail.com>
2025-01-07 13:10:16 -0500

    reducer and store and everything else is in the main.jsx file yet.

--- part5-State-Management/redux-counter/src/App.jsx
+++ part5-State-Management/redux-counter/src/App.jsx
@@ -1,17 +1,38 @@
+import { createStore } from "redux";
+
 const App = () => {
   const counterReducer = (state = 0, action) => {
     switch (action.type) {
       case "INCREMENT":
+        console.log("INCREMENT action being processed...");
         return state + 1;
       case "DECREMENT":
+        console.log("DECREMENT action being processed...");
         return state - 1;
       case "ZERO":
+        console.log("RESET(ZERO) action being processed...");
         return 0;
 
       default:
         return state;
     }
   };
+
+  const store = createStore(counterReducer);
+  store.subscribe(() => {
+    console.log("Store after action applied: ", store.getState());
+  });
+  console.log("Store State: ", store.getState());
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  console.log("Store State: ", store.getState());
+  store.dispatch({ type: "DECREMENT" });
+  store.dispatch({ type: "DECREMENT" });
+  console.log("Store State: ", store.getState());
+  store.dispatch({ type: "ZERO" });
+  console.log("State, after dispatching ZERO action: ", store.getState());
 
   return (
     <div>
--- part5-State-Management/redux-counter/src/main.jsx
+++ part5-State-Management/redux-counter/src/main.jsx
@@ -1,4 +1,57 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createStore } from "redux";
+// import App from "./App.jsx";
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      console.log("INCREMENT action being processed...");
+      return state + 1;
+    case "DECREMENT":
+      console.log("DECREMENT action being processed...");
+      return state - 1;
+    case "ZERO":
+      console.log("RESET(ZERO) action being processed...");
+      return 0;
 
-createRoot(document.getElementById("root")).render(<App />);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+store.subscribe(() => {
+  console.log("Store state after the dispatch: ", store.getState());
+  renderApp();
+});
+
+const App = () => {
+  // console.log("Store State: ", store.getState());
+  // store.dispatch({ type: "INCREMENT" });
+  // store.dispatch({ type: "INCREMENT" });
+  // store.dispatch({ type: "INCREMENT" });
+  // store.dispatch({ type: "INCREMENT" });
+  // console.log("Store State: ", store.getState());
+  // store.dispatch({ type: "DECREMENT" });
+  // store.dispatch({ type: "DECREMENT" });
+  // console.log("Store State: ", store.getState());
+  // store.dispatch({ type: "ZERO" });
+  // console.log("State, after dispatching ZERO action: ", store.getState());
+
+  return (
+    <div>
+      <h2>Hello Redux.</h2>
+      <p>Counter: {store.getState()} </p>
+      <button onClick={() => store.dispatch({ type: "DECREMENT" })}>-</button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>Reset</button>
+      <button onClick={() => store.dispatch({ type: "INCREMENT" })}>+</button>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+const renderApp = () => {
+  root.render(<App />);
+};
+renderApp();
+
+// createRoot(document.getElementById("root")).render(<App />);
